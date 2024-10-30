ce1d92edd678099d3871c739f0234e1da3bf5f6c
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2024-10-30 20:12:41 +0300

    Boring Apartments.py
    https://codeforces.com/problemset/problem/1433/A

---
+++ Boring Apartments.py
@@ -0,0 +1,15 @@
+'''
+https://codeforces.com/problemset/problem/1433/A
+A. Boring Apartments
+'''
+SUM = lambda x: int((x*(x+1)/2))
+
+t = int(input())
+
+for _ in range(t):
+      x = input()
+      rem =  len(x) % 4
+      if rem == 0:
+            print((int(x[0])-1)*10+10)
+      else:
+            print((int(x[0])-1)*10+SUM(rem))
