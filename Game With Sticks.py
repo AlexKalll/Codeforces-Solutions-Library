60d87659eeed99d5cc587efbadb75f1e8f9f5afc
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-01-11 22:58:39 +0300

    Game With Sticks.py
    https://codeforces.com/problemset/problem/451/A

---
+++ Game With Sticks.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/problemset/problem/451/A
+
+h, v = sorted(list(map(int, input().split())))
+sticks = h + v
+points = h * v
+winner = 0
+
+while points != 0:
+      if h != 0:
+            h -= 1
+            points = h * v
+            winner += 1
+      elif v != 0:
+            v -= 1
+            points = h * v
+            winner += 1
+
+print('Akshat') if (winner % 2 == 1) else print('Malvika') 
